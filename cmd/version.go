b0db78dd1f6de537e23ba3ae740e1424aa89030d
Bill Burke <bburke@keycloak.ip>
2018-04-01 14:23:21 -0400

    bump version

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ var versionCmd = &cobra.Command{
 }
 
 func version(cmd *cobra.Command, args []string) {
-	console.Writeln("kcinit version \"0.4\"")
+	console.Writeln("kcinit version \"0.5\"")
 }
 
 func init() {
